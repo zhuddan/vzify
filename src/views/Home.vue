45566c824d7c60ffcababb24e4d90d4923da8e43
zhuddan <476182045@qq.com>
2022-12-06 17:24:29 +0800

    1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,13 +4,15 @@ const isActive = ref(false);
 
 <template>
   <v-container>
-    <v-card
-      v-for="item in 100" :key="item"
-      width="400"
-      title="This is a title"
-      subtitle="This is a subtitle"
-      text="This is content"
-    />
+    <v-spacer>
+      <v-card
+        v-for="item in 100" :key="item"
+        width="400"
+        title="This is a title"
+        subtitle="This is a subtitle"
+        text="This is content"
+      />
+    </v-spacer>
   </v-container>
 </template>
 
