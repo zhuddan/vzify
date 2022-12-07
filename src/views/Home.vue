f1bd43aad8d5f7fa542cca90f58cdc8b0b8c24ad
zhuddan <476182045@qq.com>
2022-12-07 23:25:48 +0800

    1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,78 @@
 <script setup lang="ts">
 const elevations = [0, 12, 18];
+const tab = ref(null);
 </script>
 
 <template>
-  <v-container class="fill-height">
-    <v-sheet
-      class="pa-12 "
-      color="grey-lighten-3"
-    />
+  <v-container>
+    <v-toolbar
+      dark
+      absolute
+      style="z-index: 10"
+    >
+      <v-tabs
+        v-model="tab"
+        class="full-height ma-2"
+      >
+        <v-tab value="one">
+          最新
+        </v-tab>
+        <v-tab value="two">
+          最多查看
+        </v-tab>
+        <v-tab value="three">
+          最多收藏
+        </v-tab>
+      </v-tabs>
+    </v-toolbar>
+
+    <v-window v-model="tab">
+      <v-window-item value="one" class="">
+        <v-card
+          v-for="it in 10"
+          :key="it"
+          elevation="2"
+          title="Card title"
+          subtitle="Subtitle"
+          text="..."
+          class=" ma-2"
+        >
+          <v-card-actions>
+            <v-btn>Click me</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-window-item>
+      <v-window-item value="two">
+        <v-card
+          v-for="it in 10"
+          :key="it"
+          elevation="2"
+          title="Card title"
+          subtitle="Subtitle"
+          text="..."
+          class=" ma-2"
+        >
+          <v-card-actions>
+            <v-btn>Click me</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-window-item>
+      <v-window-item value="three">
+        <v-card
+          v-for="it in 10"
+          :key="it"
+          elevation="2"
+          title="Card title"
+          subtitle="Subtitle"
+          text="..."
+          class=" ma-2"
+        >
+          <v-card-actions>
+            <v-btn>Click me</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-window-item>
+    </v-window>
   </v-container>
 </template>
 
